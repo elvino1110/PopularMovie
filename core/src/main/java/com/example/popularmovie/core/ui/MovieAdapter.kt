961304a8b73c3d3e5a9188a7c634b4b87b8a9ed9
elvino <12050111660@students.uin-suska.ac.id>
2023-11-06 12:18:50 +0700

    warning fix

--- core/src/main/java/com/example/popularmovie/core/ui/MovieAdapter.kt
+++ core/src/main/java/com/example/popularmovie/core/ui/MovieAdapter.kt
@@ -3,6 +3,7 @@ package com.example.popularmovie.core.ui
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.popularmovie.core.R
@@ -15,12 +16,35 @@ class MovieAdapter : RecyclerView.Adapter<MovieAdapter.ListViewHolder>() {
     private var listData = ArrayList<Movie>()
     var onItemClick: ((Movie) -> Unit)? = null
 
+    companion object {
+        private val diffCallback = object : DiffUtil.ItemCallback<Movie>() {
+            override fun areItemsTheSame(oldItem: Movie, newItem: Movie) =
+                oldItem.id == newItem.id
+
+            override fun areContentsTheSame(oldItem: Movie, newItem: Movie) =
+                oldItem == newItem
+        }
+    }
+
     fun setData(newListData: List<Movie>?) {
         if (newListData == null) return
+
+        val diffResult = DiffUtil.calculateDiff(object : DiffUtil.Callback() {
+            override fun getOldListSize() = listData.size
+            override fun getNewListSize() = newListData.size
+            override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int) =
+                listData[oldItemPosition].id == newListData[newItemPosition].id
+
+            override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int) =
+                listData[oldItemPosition] == newListData[newItemPosition]
+        })
+
         listData.clear()
         listData.addAll(newListData)
-        notifyDataSetChanged()
+
+        diffResult.dispatchUpdatesTo(this)
     }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
         ListViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_movie, parent, false))
@@ -53,4 +77,6 @@ class MovieAdapter : RecyclerView.Adapter<MovieAdapter.ListViewHolder>() {
             }
         }
     }
+
+
 }
